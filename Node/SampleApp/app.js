8063440810d42bae9f6bbbe7705fb7ba5a2daea3
David Shilkret <dshilkret@gmail.com>
2021-06-29 14:45:55 -0500

    init

--- Node/SampleApp/app.js
+++ Node/SampleApp/app.js
@@ -1,9 +1,9 @@
 const restify = require('restify');
 const CookieParser = require('restify-cookies');
 
-process.env.AUTH_CLIENT_ID = (process.env.AUTH_CLIENT_ID) ? process.env.AUTH_CLIENT_ID : '[auth client ID]';
-process.env.AUTH_CLIENT_SECRET = (process.env.AUTH_CLIENT_SECRET) ? process.env.AUTH_CLIENT_SECRET : '[auth client secret]';
-process.env.BASE_URI = (process.env.BASE_URI) ? process.env.BASE_URI : '[the host name for your application]';
+process.env.AUTH_CLIENT_ID = (process.env.AUTH_CLIENT_ID) ? process.env.AUTH_CLIENT_ID : '3a87003e-95ef-4655-ac60-466f9a9efe10';
+process.env.AUTH_CLIENT_SECRET = (process.env.AUTH_CLIENT_SECRET) ? process.env.AUTH_CLIENT_SECRET : '.vCv70l_CAM33ERBlf_R2wv90Y~bbeDGs_';
+process.env.BASE_URI = (process.env.BASE_URI) ? process.env.BASE_URI : 'docintegrator.onmicrosoft.com';
 
 var server = restify.createServer();
 server.use(restify.queryParser());
